6958d8fe865716cdde68a2ba71b1c0a653d93c19
Nadi Duno <nadiduno@gmai.com>
2023-09-03 22:41:47 -0300

    Withdrawal

--- main.py
+++ main.py
@@ -29,6 +29,21 @@ while True:
 
 
     elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo sufuciente.")
+        elif excedeu_saldo:
+            print("Operação falhou! O valor do saque excede o limite")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor: .2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou! O valor informado é inválido")
 
     elif opcao == "e":
 

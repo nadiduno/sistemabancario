65b177566e86d2d124338d7d33e4a9fc9e45008f
Nadi Duno <nadiduno@gmai.com>
2023-09-03 22:50:35 -0300

    Statement

--- main.py
+++ main.py
@@ -46,6 +46,10 @@ while True:
             print("Operação falhou! O valor informado é inválido")
 
     elif opcao == "e":
+        print("\n============= EXTRATO =============")
+        print("Não foram realizados movimentações" if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo: .2f}")
+        print("============================")
 
     elif opcao == "q":
         break
